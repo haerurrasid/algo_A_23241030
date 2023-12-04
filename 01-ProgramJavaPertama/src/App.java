f4c6cd0a4dfb7f8c0fc971df73a69b7f00e68697
haerurrasid <145946530+haerurrasid@users.noreply.github.com>
2023-12-04 12:49:15 +0800

    TUGAS ALGO PERTAMA

---
+++ 01-ProgramJavaPertama/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //membuat sebuah variabel dan tipe data//
+
+        String nama;
+        String nim;
+        String prodi;
+       
+
+        nama = "HAERUR RASID";
+        nim = "23241030";
+        prodi = "PENDIDKAN TEKNOLOGI INFORMASI";
+        
+        
+
+        //mengeprint
+
+        System.out.println("Nama Lengkap :" + nama);
+        System.out.println("Nim Mahasiswa :"+ nim);
+        System.out.println("Prodi :"+ prodi);
+        }
+}
