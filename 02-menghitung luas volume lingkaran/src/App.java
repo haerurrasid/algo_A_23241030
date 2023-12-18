b29e8f6d9a4097dd0f2221c6c21448a384362e36
haerurrasid <145946530+haerurrasid@users.noreply.github.com>
2023-12-18 01:45:54 -0800

    tugas modul pertanyaan 1

---
+++ 02-menghitung luas volume lingkaran/src/App.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class App static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        float phi = 3.14f;
+        float jari, LuasP, volume;
+
+        System.out.println("\t Hitung Luas & volume bola dengan method \n\n");
+
+        System.out.print(" Masukan nilai jari-jari :");
+        jari = userInput.nextFloat();
+
+        System.out.println("Hasilnya adalah.....");
+        System.out.print("===============");
+
+        LuasP = LuasPermukaan(jari);
+        System.out.println("\n Luas Permukaanya :" + LuasP);
+
+        volume = volumeBola(jari);
+        System.out.println("volume Bolanya      :" + volume);
+
+    }
+
+    public static float LuasPermukaan(float r) {
+        float phi = 3.14f;
+        float hasil = 4 * phi * r * r;
+
+        return hasil;
+
+    }
+
+    public static float volumeBola(float r) {
+        float phi = 3.14f;
+        float hasil = 4 / (float) 3 * phi * r * r;
+
+        return hasil;
+    }
+}
